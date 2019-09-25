a76096ea81289d675f56d602eb38c4f82d6ecf59
AnthonyBianco <anthonysilvanobianco@gmail.com>
2019-09-25 22:11:54 +0000

    working with Darren

--- play.js
+++ play.js
@@ -1,18 +1,5 @@
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  const handleUserInput = (data) => {
-    if (data === '\u0003'){
-      process.exit();
-    }
-  }
-  stdin.on ('data', handleUserInput);
-}
-
 const { connect } = require('./client');
-
+const { setupInput } = require('./input');
 console.log('Connecting ...');
 connect();
 setupInput();
