42d3afd98339bce0348431e7d5cecf43284d7e6d
AnthonyBianco <anthonysilvanobianco@gmail.com>
2019-09-25 21:59:05 +0000

    working with Darren

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 const net = require('net');
 
+
 const connect = function() {
   const conn = net.createConnection({ 
     host: 'localhost',
@@ -11,26 +12,6 @@ const connect = function() {
     conn.write("Name: Tom" );
 
   });
-  // conn.on('connect', () => {
-  //   setInterval(function(){     
-  //   conn.write("Move: up");
-  //   }, 1000);
-  //   setInterval(function(){     
-  //     conn.write("Move: up");
-  //   }, 2000);
-  //   setInterval(function(){     
-  //     conn.write("Move: up");
-  //   }, 3000);
-  //   setInterval(function(){     
-  //     conn.write("Move: left");
-  //   }, 4000);
-  //   setInterval(function(){     
-  //     conn.write("Move: right");
-  //   }, 4000);
-
-  });
-
-
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
--- play.js
+++ play.js
@@ -1,4 +1,18 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = (data) => {
+    if (data === '\u0003'){
+      process.exit();
+    }
+  }
+  stdin.on ('data', handleUserInput);
+}
+
 const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
+setupInput();
